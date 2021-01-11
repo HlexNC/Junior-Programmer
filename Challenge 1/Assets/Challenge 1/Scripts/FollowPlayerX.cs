497df7daef376475ce90c2542f3ec970567a7ccc
HlexNC <rudaevalex@gmail.com>
2021-01-11 18:29:42 +0300

    Last time forgot to add my

---
+++ Challenge 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Challenge_1.Scripts
+{
+    public class FollowPlayerX : MonoBehaviour
+    {
+        public GameObject plane;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = plane.transform.position + new Vector3(20, 3, 5);
+        }
+    }
+}
---
+++ Challenge 1/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Challenge_1.Scripts
+{
+    public class SpinPropellerX : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(Vector3.forward, 200.0f);
+        }
+    }
+}
